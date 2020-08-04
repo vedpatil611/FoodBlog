735af0359954becbfaf9065684ed2ed9ea7af52d
vedpatil611 <vedpatil611@gmail.com>
2020-08-04 23:39:39 +0530

    database connectivity

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<?php
+  require "conn.php"
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>The Hungry Chipmunks</title>
-    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="public/stylesheets/styles.css" />
     <link
       href="https://fonts.googleapis.com/css?family=Oxygen:400,300,700"
       rel="stylesheet"
