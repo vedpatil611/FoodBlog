f49520cbb7a9baf65188f10745b9c88a974ba18f
Dion Pinto <dionpinto000@gmail.com>
2020-08-20 15:18:01 +0530

    added view and slide

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>The Hungry Chipmunks</title>
-    <link rel="stylesheet" href="public/stylesheets/styles.css?version=51" />
+    <link rel="stylesheet" href="public/stylesheets/styles.css?version=53" />
     <link
       href="https://fonts.googleapis.com/css?family=Oxygen:400,300,700"
       rel="stylesheet"
