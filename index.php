879066aa43eebdeb221c15077cfdca09c5da8eae
Dion Pinto <dionpinto000@gmail.com>
2020-08-27 15:28:09 +0530

    added additional styling and profile page

--- index.php
+++ index.php
@@ -52,33 +52,7 @@
             <input  type="text" placeholder="Search">
             <button>&#x2315;</button>
           </div>
-           <!-- Slideshow container -->
-           <div class="slideshow-container">
-
-            <!-- Full-width images with number and caption text -->
-            <div class="mySlides fade">
-              <div class="numbertext">1 / 3</div>
-              <img src="res/images/background-cake.jpg" style="width:100%">
-              <div class="text">Caption Text</div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">2 / 3</div>
-              <img src="res/images/phone-660.jpg" style="width:100%">
-              <div class="text">Caption Two</div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">3 / 3</div>
-              <img src="res/images/phone-660.jpg" style="width:100%">
-              <div class="text">Caption Three</div>
-            </div>
-
-            <!-- Next and previous buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
-            </div>
-            <br>
+           
           <div class="content">
             <div class="content-item">
               <div>
---
+++ profile.php
@@ -0,0 +1,88 @@
+<?php
+  require "conn.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>The Hungry Chipmunks</title>
+    <link rel="stylesheet" href="public/stylesheets/styles.css?version=53" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Oxygen:400,300,700"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Lora"
+      rel="stylesheet"
+      type="text/css"
+    />
+  </head>
+  <body>
+    <div class="grid-container">
+        <header class="header">
+          <div class="brand">
+            <button onClick="openMenu()">&#9776</button>
+            <a href="index.php">The Hungry Chipmunks</a>
+          </div>
+          <div class="header-links">
+            <a href="index.php">Home</a>
+            <a href="login.php">Log In</a>
+            <a href="signup.php">Sign Up</a>
+          </div>
+        </header>
+        <aside class="sidebar">
+          <h3>Menu</h3>
+          <button class="sidebar-close-button"  onClick="closeMenu()">
+            &#10005
+          </button>
+          <ul>
+            <li>
+              <a href="index.php">Recepies</a>
+            </li>
+            <li>
+              <a href="index.php">Blog</a>
+            </li>
+          </ul>
+        </aside>
+        <main class="main">
+          <div class="search">
+            <input  type="text" placeholder="Search">
+            <button>&#x2315;</button>
+          </div>
+          <div class="content-profile">
+
+            <div class="content-profile-item">
+       
+            <div>UserName</div> 
+            <div class="profile-btn">
+             
+            </div>
+            </div>
+
+            <div class="content-profile-item">
+            <img src="res/images/phone-660.jpg">
+          
+            </div>
+            
+          </div>
+        </main>
+
+
+
+
+        <footer class="footer">&copy;ChipmunksCo</footer>
+      </div>
+      <script>
+        function openMenu(){
+          document.querySelector(".sidebar").classList.add("open")
+        }
+  
+        function closeMenu(){
+          document.querySelector(".sidebar").classList.remove("open")
+        }
+      </script>
+  </body>
+</html>
