916d3707e3a65911bb3e7d892f13d69bbfcfc780
vedpatil611 <vedpatil611@gmail.com>
2020-10-22 13:56:50 +0530

    Forget password

--- password_recovery.php
+++ password_recovery.php
@@ -9,9 +9,11 @@
       CALL delete_rows_links();
     */
       
- 	function generateLink() {
+ 	function generateOTP($userID) {
  		$otp = rand(1000, 9999);
-
+ 		$sql = "INSERT INTO otp(id, OTP) VALUES(".$userID.", ".$otp.")";
+ 		mysqli_query($conn, $sql);
+ 		return $otp;
  	}
 
  	$foodblog_email = "";
@@ -22,16 +24,34 @@
 	 	$email = $_POST["email"];
 	 	$username = $_POST["username"];
 
+	 	$id = 0;
+
+	 	$sql = "SELECT id FROM users WHERE username=`".$username."` AND email=`".$email."`";
+		$result = mysqli_query($conn, $sql);
+
+		if(mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				$id = $row["id"];
+				break;
+			}
+		} else {
+			/*
+			 *	Username not found or email not found
+			 */
+		}
+
+		$otp = generateOTP($id);
+
 		$transport = (new Swift_SmtpTransport('smtp.gmail.com', 25))
 			->setUsername($foodblog_email)
 	  		->setPassword($foodblog_pass);
 
 		$mailer = new Swift_Mailer($transport);
 
-		$message = (new Swift_Message('Forgot password'))
+		$message = (new Swift_Message('Password reset'))
 			->setFrom([$foodblog_email => 'FoodBlog'])
 			->setTo([$email])
-			->setBody('');
+			->setBody('Enter otp to reset password: '.$otp);
 
 		$result = $mailer->send($message);
 	}
